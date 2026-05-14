ec8ffd4b2dc5234c959cf48c2c49bcc48004ab69
saurabh shinde <saurabhbshinde858@gmail.com>
2026-05-14 08:42:00 +0530

    added for ...

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+         {/* <div className='text-5xl flex justify-center items-center h-screen'>hello</div> */}
     <App />
   </StrictMode>,
 )

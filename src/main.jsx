62195509b4678a7f5fb43100761079fb52e8228b
saurabh shinde <saurabhbshinde858@gmail.com>
2026-05-14 15:39:30 +0530

    add post event line

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-         {/* <div className='text-5xl flex justify-center items-center h-screen'>hello</div> */}
+         {/* <div className='text-5xl flex justify-center px-7 py-4 items-center h-screen'>कार्यक्रम संपला आहे. पुढच्या वर्षी भेटूया!🎉  </div> */}
     <App />
   </StrictMode>,
 )
